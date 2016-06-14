b64475fa8483daecf71198afb1f34a46fd04fb34
Steve Lawford <steve_lawford@yahoo.co.uk>
2016-06-14 10:47:12 +0200

    Minor formatting changes

--- build/code/state_state_border_dictionary.py
+++ build/code/state_state_border_dictionary.py
@@ -12,6 +12,7 @@ def build_dict():
     f = open(src, 'r')
 
     for header in f:
+        
         break
     
     for line in f:
@@ -19,9 +20,12 @@ def build_dict():
         line_v = line.split(',')
         border_num = int(line_v[0])
         
-        if border_num not in border_dict:  
-            border_dict[border_num] = [[-float(line_v[4])], [float(line_v[3])]] 
+        if border_num not in border_dict:
+            
+            border_dict[border_num] = [[-float(line_v[4])], [float(line_v[3])]]
+            
         else:
+            
             border_dict[border_num][0].append(-float(line_v[4]))
             border_dict[border_num][1].append(float(line_v[3]))
     
