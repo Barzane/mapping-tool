3e2fb1797c96a604bb2d4e6270be65399d33e5fd
Steve Lawford <steve_lawford@yahoo.co.uk>
2016-06-16 19:15:48 +0200

    Update version of module
     - this module is used in multiple directories // to resolve

--- build/code/access_external_functions.py
+++ build/code/access_external_functions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os, sys, inspect
+import os, sys, inspect, re
 
 def set_path():
 
@@ -14,12 +14,56 @@ def set_path():
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     uppath = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
     externaldir = uppath(currentdir, 3) + '\\network-data\\build\\code'
-    sys.path.insert(0, externaldir)
     
-    print '\nexternaldir', externaldir
+    def check_duplicate_names():
+
+        def searcher(all_files):
+            
+            python_files = list()
+            
+            for file in all_files:
+            
+            #    https://developers.google.com/edu/python/regular-expressions#basic-examples
+                
+                lookup_py = re.search(r'.py', file)
+                lookup_pyc = re.search(r'.pyc', file)
+                
+            #    http://stackoverflow.com/questions/23086383/how-to-test-nonetype-in-python
+                
+                if (lookup_py is not None and lookup_pyc is None):
+                    
+                    python_files.append(file)        
+            
+            return python_files
+        
+        list_current_files = os.listdir(currentdir)        
+        list_current_python_files = searcher(list_current_files)
+        
+        list_external_files = os.listdir(externaldir)
+        list_external_python_files = searcher(list_external_files)
+        
+        for file in list_current_python_files:
+            
+            if file in list_external_python_files:
+                
+                if file != 'rundirectory.py':
+                    
+                    return True
+                    
+        return False
     
-    #remove duplicates from sys.path --- be careful when changing sys.path
+    if check_duplicate_names():
+        
+        raise Exception('repeated module name in:', currentdir, externaldir)
     
-    sys.path = list(set(sys.path))
+    else:
+    
+        sys.path.insert(0, externaldir)
+        
+        print '\nexternaldir', externaldir
+        
+        #remove duplicates from sys.path --- be careful when changing sys.path
+        
+        sys.path = list(set(sys.path))
 
     return None
